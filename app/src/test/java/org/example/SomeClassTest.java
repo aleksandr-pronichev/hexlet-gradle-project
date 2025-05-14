5beb39f43259a0fcb272e2ae8b37b905d8503870
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2025-05-14 21:53:00 +0300

    initial commit

---
+++ app/src/test/java/org/example/SomeClassTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class SomeClassTest {
+    @Test
+    public void testSum() {
+	var expected = 5;
+	var actual = SomeClass.sum(3, 2);
+	assertEquals(expected, actual);
+    }
+}
